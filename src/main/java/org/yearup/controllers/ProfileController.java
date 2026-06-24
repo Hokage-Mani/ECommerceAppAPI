0933276efb7650432885a99ddc1b8837a614ce60
Hokage-Mani <armaniburton.nyc@gmail.com>
2026-06-24 15:34:26 -0400

    Finalized submission of my API, went over the checklist to meet criteria of the processes that required bug squashing and implementing User access and login.

--- src/main/java/org/yearup/controllers/ProfileController.java
+++ src/main/java/org/yearup/controllers/ProfileController.java
@@ -27,24 +27,22 @@ public class ProfileController {
 
     @GetMapping
     public Profile getProfile(Principal principal) {
-        // 1. Get the username of the person logged in
+        // Get the username of the person logged in
         String username = principal.getName();
-        // 2. Find their User object to get their ID
+        // Find their User object to get their ID
         User user = userService.getByUserName(username);
-        // 3. Return their specific profile
+        // Return their specific profile
         return profileService.getProfileByUserId(user.getId());
     }
 
     @PutMapping
     public Profile updateProfile(Principal principal, @RequestBody Profile profile) {
-        // 1. Get the username and User object
+        // Get the username and User object
         String username = principal.getName();
         User user = userService.getByUserName(username);
 
-        // 2. Lock the profile's User ID to the logged-in user so they can't hack someone else's profile
+        // Lock the profile's User ID to the logged-in user so they can't hack someone else's profile
         profile.setUserId(user.getId());
-
-        // 3. Update the profile
         return profileService.updateProfile(profile);
     }
 }
